ff69d58161656aa042bb96bcf17b0ab832c0d5c3
MattDyer87 <71666354+MattDyer87@users.noreply.github.com>
2021-06-06 11:28:14 -0700

    Building File IO

--- DLinkedList/FileIO.java
+++ DLinkedList/FileIO.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.File;
+import java.io.FileWriter;
+import java.util.Random;
 
 /**
  * Write a description of class FileIO here.
@@ -11,14 +13,36 @@ import java.io.File;
  */
 public class FileIO
 {
+    public static final Random rnd = new Random();
+    
     public static void main(String[] args) {
-        readFile("ssss");
+        //readFile("ssss");
+        writeText("numbers.txt");
     }
     
     public static void readFile(String name) {
-        Files.lines(new File("input.txt").toPath())
-                    .map(s -> s.trim())
-                    .filter(s -> !s.isEmpty())
-                    .forEach(System.out::println);
+        try {
+            Files.lines(new File("input.txt").toPath())
+                        .map(s -> s.trim())
+                        .filter(s -> !s.isEmpty())
+                        .forEach(System.out::println);
+        }
+        catch(IOException e) { 
+            e.printStackTrace();
+        }
+    }
+    
+    public static void writeText(String name)
+    {
+        try {
+            FileWriter writer = new FileWriter(name, true);
+            for(int i = 0; i < 10; i++) {
+                writer.write(rnd.nextInt(100 + 1));
+                writer.write("\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
